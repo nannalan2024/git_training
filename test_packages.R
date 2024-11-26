68a807da1dd678039c2e8eddf62e5d99ee7a0be1
nannalan2024 <nanna.lan@outlook.com>
2024-11-26 18:31:42 +0000

    ja

--- test_packages.R
+++ test_packages.R
@@ -2,3 +2,4 @@ library(RPostgres)
 library(tidyverse) # Load many packages often used in data science
 library(MASS)
 
+# Jeg prøver at skrive noget andet her for at teste, om jeg kan merge filerne nu
